3cb1e4c011d297c312367eda17f7a72dd0c76d0f
Keïsha Alexander <90068272+K3A101@users.noreply.github.com>
2024-01-02 14:39:45 +0100

    Uploaded assignment 1

---
+++ opdracht-1/static/js/main.js
@@ -0,0 +1,12 @@
+console.log("Javascript loaded");
+// const topicDetails = document.querySelectorAll("details")
+// const detailSummaries = document.querySelectorAll("summary")
+// topicDetails.forEach(detail =>{
+//     detailSummaries.forEach(summary =>{
+//         summary.addEventListener('click', ()=>{
+//             summary.classList.toggle("open");
+//
+//         })
+//     })
+//
+// })
